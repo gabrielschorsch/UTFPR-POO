a65978471515afe264b2410412469d2b3078a58f
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2022-08-12 11:42:10 -0300

    added class 01

---
+++ src/aula01/ExemploSaidaDados.java
@@ -0,0 +1,28 @@
+package aula01;
+
+public class ExemploSaidaDados {
+
+	public static void main(String [] args) {
+		
+		String disciplina = "POO";
+		String professor = "Vinícius";
+		
+		System.out.println("==== PRINTLN ====");
+		System.out.println("Universidade Tecnológica Federal do Paraná");
+		System.out.println(disciplina + " ministrada pelo Prof. " + professor);
+		
+		System.out.println("\n==== PRINT ====");
+		System.out.print("Universidade Tecnológica Federal do Paraná");
+		System.out.print(disciplina + " ministrada pelo Prof. " + professor);
+		
+		System.out.println("\n\n==== PRINTF ====");
+		System.out.printf("Universidade Tecnológica Federal do Paraná");
+		System.out.printf("%s ministrada pelo Prof. %s", disciplina, professor);
+	}
+}
+
+
+
+
+
+
