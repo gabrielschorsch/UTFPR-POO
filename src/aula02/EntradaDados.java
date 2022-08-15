1c08d97b3db10a84f91272f8a965e5597bdb6d5b
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2022-08-15 09:11:05 -0300

    added class 02

---
+++ src/aula02/EntradaDados.java
@@ -0,0 +1,33 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class EntradaDados {
+
+	public static void main(String[] args) {
+		
+		Scanner entradaTeclado = new Scanner(System.in);
+		
+		System.out.println("Informe a idade:");
+		int idade = entradaTeclado.nextInt();
+
+		entradaTeclado.nextLine(); // limpeza de buffer
+		
+		System.out.println("Informe o nome:");
+		String nome = entradaTeclado.nextLine();
+		
+		System.out.println("Informe o peso:");
+		double peso = entradaTeclado.nextDouble();
+		
+		System.out.println(nome + " possui " + idade + " anos e " + peso + "kg.");
+		
+		entradaTeclado.close();
+	}
+}
+
+
+
+
+
+
+
